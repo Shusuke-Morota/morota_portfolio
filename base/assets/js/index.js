158c8bacd9d39f32cd9e049897a7a6a2905189e2
Shusuke-Morota <shusuke.7.tommy@gmail.com>
2022-02-16 01:22:02 +0900

    [add:] 各種ファイルにおいて、ハンバーガーメニューを追加。

---
+++ base/assets/js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+  const $drawerToggle = $(".js-drawerToggle");
+
+  function activeDrawer() {
+    $drawerToggle.toggleClass("active");
+  }
+
+  function activeNav() {
+    const $nav = $(".js-navigation");
+    $nav.toggleClass("active");
+  }
+
+  function fixedBody() {
+    const $body = $("body");
+    $body.toggleClass("active");
+  }
+
+  function init() {
+    activeDrawer();
+    activeNav();
+    fixedBody();
+  }
+
+  $drawerToggle.on("click", init);
+});
