765d2e7d756b7adf1f7eb49ff636a0674186b253
Shusuke-Morota <shusuke.7.tommy@gmail.com>
2022-02-18 11:17:15 +0900

    [add:] 各種ファイルにおいて、sectionに差し掛かるとsectionが下からフェードアップするスクロールイベントを追加。

--- base/assets/js/index.js
+++ base/assets/js/index.js
@@ -15,11 +15,38 @@ $(function () {
     $body.toggleClass("active");
   }
 
-  function init() {
+  function fadeUpAnimation() {
+    const $fadeUpTrigger = (".fadeUpTrigger")
+    $($fadeUpTrigger).each(function() { //fadeUpTriggerというクラス名が
+      const $elementPosition = $(this).offset().top-50;//要素より、50px上の
+      const $scroll = $(window).scrollTop();
+      const $sectionHeight = $(window).height();
+      if ($scroll >= $elementPosition - $sectionHeight) {
+      $(this).addClass("fadeUp");// 画面内に入ったらfadeUpというクラス名を追記
+      }
+    });
+  }
+
+  function hamburgerMenu() {
     activeDrawer();
     activeNav();
     fixedBody();
   }
 
-  $drawerToggle.on("click", init);
+  function setEvent() {
+    $drawerToggle.on("click", hamburgerMenu)
+  }
+
+  function scrollEvent() {
+    $(window).scroll(function() {
+      fadeUpAnimation();
+    });
+  }
+
+  function init() {
+    scrollEvent();
+    setEvent();
+  }
+
+  init();
 });
